13ba0fd9bb80fbc9a4f47a50b38558a80f9b4180
hbheng <hb988651@gmail.com>
2024-01-19 15:47:25 +0700

    before merge main

--- backend/derleng/views.py
+++ backend/derleng/views.py
@@ -67,4 +67,21 @@ class CategoryAPIView(APIView):
         category_id.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
     
+
+class ReviewPostAPIView(APIView):
+    def post(self  , request , *args, **kwargs):
+        data = request.data
+        serializers = ReviewSerializer(data=data)
+        if serializers.is_valid():
+            if request.user.is_authenticated:
+                review = Review (
+                    user_id = request.user,
+                    package_id  = data.get('package_id'),
+                    rating = data.get('rating'),
+                    comment = data.get('comment'),
+                )
+            
+            review.save()
+            return Response({'messgae' : 'Successfully'} , status=status.HTTP_201_CREATED)
+        return Response(serializers.errors , status=status.HTTP_400_BAD_REQUEST)
         
