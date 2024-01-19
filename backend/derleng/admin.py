b12c5add945ac8ccfbf0d82cb295451bd3168bfe
hbheng <hb988651@gmail.com>
2024-01-19 17:06:30 +0700

    before merge main

--- backend/derleng/admin.py
+++ backend/derleng/admin.py
@@ -1,85 +1,63 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Guide_register_info, Profile_image, Category, Package, Package_image, Package_schedule, Package_service, Package_unavailable_date, Payment_method, Cart, Booking, Review, Thumbnail
 
-from django.contrib import admin
-from .models import *
+@admin.register(Guide_register_info)
+class GuideRegisterInfoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'fullname_khmer', 'phone', 'address')
 
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username', 'fullname', 'email', 'Phone', 'last_login', 'is_active', 'created_at')
-    search_fields = ('username', 'email', 'Phone')
-    list_filter = ('is_active', 'created_at')
-
-admin.site.register(User, UserAdmin)
-
-class UserRoleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description')
-    search_fields = ('name', 'description')
-    
-admin.site.register(User_role, UserRoleAdmin)
-
-class ProfilePictureAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'user_id' , 'type' , 'is_active' , 'created_at')
+@admin.register(Profile_image)
+class ProfileImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'type', 'is_active', 'created_at')
     list_filter = ('is_active', 'created_at' , 'type')
 
-admin.site.register(Profile_picture , ProfilePictureAdmin)
-
-class CategoryAdmin (admin.ModelAdmin):
-    list_display = ('id' , 'name')
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
     search_fields=('name',)
-    
-admin.site.register(Category, CategoryAdmin)
-    
-class PackageAdmin (admin.ModelAdmin):
-    list_display = ('id' , 'description' , 'user_id' , 'category_id', 'discount' , 'location_coordination' , 'video_url')
+
+@admin.register(Package)
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_id', 'description', 'category_id', 'discount', 'address', 'video_url')
     search_fields = ( 'description' , )
-admin.site.register(Package , PackageAdmin)
 
+@admin.register(Package_image)
 class PackageImageAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'package_id' , 'uri_image')
-admin.site.register(Package_image , PackageImageAdmin)
+    list_display = ('id', 'package_id', 'image')
 
+@admin.register(Package_schedule)
 class PackageScheduleAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'package_id' , 'destination' , 'start_time' , 'end_time')
-    search_fields = ( 'destination' ,)
-admin.site.register(Package_schedule , PackageScheduleAdmin)
+    list_display = ('id', 'package_id', 'destination', 'start_time', 'end_time')
 
+@admin.register(Package_service)
 class PackageServiceAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'name' , 'package_id' , 'price')
+    list_display = ('id', 'detail', 'package_id', 'price')
     search_fields = ('name',)
     list_filter = ( 'price' ,)
-admin.site.register(package_service,PackageServiceAdmin)
-    
-    
-class PaymentmethodAdmin(admin.ModelAdmin):
-    list_display =('id' , 'type' , 'name' , 'number' , 'expire_date' , 'cvv')
-    search_fields = ( 'name' , 'number')
+
+@admin.register(Package_unavailable_date)
+class PackageUnavailableDateAdmin(admin.ModelAdmin):
+    list_display = ('id', 'package', 'unavailable_at')
+
+@admin.register(Payment_method)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'type', 'number', 'holder_name', 'token')
+    search_fields = ( 'holder_name' , 'number')
     list_filter=('type' ,)
 
-admin.site.register(Payment_method, PaymentmethodAdmin)
-
+@admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'user_id' , 'service_id' , 'customer_ammount', 'booking_date' , 'created_at')
-    list_filter = ('customer_ammount' , 'booking_date' , 'created_at')
-    
-admin.site.register(Cart , CartAdmin)
+    list_display = ('id', 'user', 'service', 'customer_ammount', 'booking_date', 'created_at')
 
+@admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
-    list_display = ('id' , 'cart_id' , 'is_accept' , 'created_at')
-    list_filter = ('is_accept' , 'created_at')
-    
-admin.site.register(Booking , BookingAdmin)
+    list_display = ('id', 'cart_id', 'is_accept', 'created_at')
 
+@admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display=('id' ,'user_id' , 'package_id' , 'rating' , 'comment' , 'created_at')
-    list_filter=('rating' , 'created_at')
-admin.site.register(Review,ReviewAdmin)
+    list_display = ('id', 'user_id', 'package_id', 'rating', 'comment', 'created_at')
 
+@admin.register(Thumbnail)
 class ThumbnailAdmin(admin.ModelAdmin):
-    list_display = ( 'id' , 'name', 'uri_image' , 'order_number')
-    search_fields = ( 'name' , 'order_number')
-    
-admin.site.register(Thumbnail, ThumbnailAdmin)
-    
-
-
+    list_display = ('id', 'name', 'image', 'order_number')
